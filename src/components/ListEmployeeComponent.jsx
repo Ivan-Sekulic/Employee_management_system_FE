cdac5e660e34baed9273dfafbd188e9f4c4697d9
ivan-sekulic <ivan.sekulic@neotechsolutions.org>
2022-07-11 16:58:55 +0200

    added update route

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import EmployeeService from '../services/EmployeeService';
+
 class ListEmployeeComponent extends Component {
     constructor(props){
         super(props)
         this.state = {
             employees:[]
+            
         }
         this.addEmployee = this.addEmployee.bind(this);
-
+        this.editEmployee = this.editEmployee.bind(this);
     }
     
+    editEmployee(id){
+
+        this.props.history.push(`/update-employee/${id}`);
+      }
     
     componentDidMount(){
         EmployeeService.getEmployees().then((res) => {
@@ -26,7 +32,7 @@ class ListEmployeeComponent extends Component {
             <div>
                <h2 className="text-center">Employees List</h2> 
                <div className='row'>
-                    <button className='btn btn-primary' onClick={this.addEmployee}> Add Employee</button>
+                    <button className="btn btn-success" onClick={this.addEmployee}>Add Employee</button>
                </div>
                <div className="row">
                    <table className="table table-striped table-bordered">
@@ -36,7 +42,6 @@ class ListEmployeeComponent extends Component {
                             <th> Employee Last Name</th>
                             <th> Employee Email Id</th>
                             <th> Actions</th>
-
                         </tr>
                      </thead>
                      <tbody>
@@ -47,6 +52,9 @@ class ListEmployeeComponent extends Component {
                                     <td>{employee.firstName}</td>
                                     <td>{employee.lastName}</td>
                                     <td>{employee.emailId}</td>
+                                    <td>
+                                        <button onClick={() => this.editEmployee(employee.id)} className="btn btn-info">Update</button>
+                                    </td>
                                 </tr>
                             )
                         }
--- src/components/UpdateEmployeeComponent.jsx
+++ src/components/UpdateEmployeeComponent.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import EmployeeService from '../services/EmployeeService';
 
-class createEmployeeComponent extends Component {
+class UpdateEmployeeComponent extends Component {
     constructor(props){
         super(props)
-
+        
         this.state = {
+            id: this.props.match.params.id,
             firstName: '',
             lastName: '',
             emailId: ''
@@ -13,22 +14,31 @@ class createEmployeeComponent extends Component {
 
         this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
         this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
-        this.changeEmailHandler = this.changeEmailHandler.bind(this);
 
-        this.saveEmployee = this.saveEmployee.bind(this);
+        this.updateEmployee = this.updateEmployee.bind(this);
 
     }
 
-    saveEmployee = (e) =>{ 
-        e.preventDefault();
-
-        let employee = {firstName: this.state.firstName, lastName: this.state.lastName, emailId: this.state.emailId};
-        console.log('employee => ' + JSON.stringify(employee));
-
-        EmployeeService.createEmployee(employee).then(res => {
-           this.props.history.push("/employees");
+    componentDidMount(){
+        EmployeeService.getEmployeeById(this.state.id).then((res) => {
+            let employee = res.data;
+            this.setState({firstName: employee.firstName,
+                           lastName: employee.lastName,
+                           emailId: employee.emailId
+            });
         });
     }
+
+    updateEmployee = (e) =>{ 
+        e.preventDefault();
+        let employee = {firstName: this.state.firstName, lastName: this.state.lastName, emailId: this.state.emailId};
+        console.log('employee => ' + JSON.stringify(employee));
+        EmployeeService.updateEmployee(employee, this.state.id).then( res => {
+            this.props.history.push('/employees');
+        });
+    }
+    
+
 
     changeFirstNameHandler = (event) =>{
         this.setState({firstName: event.target.value});
@@ -50,7 +60,7 @@ class createEmployeeComponent extends Component {
                   <div className='container'>
                     <div className='row'>
                         <div className='card col-md-6 offset-md-3 offset-md-3'>
-                            <h3 className='text-center'>Add Employee</h3>
+                            <h3 className='text-center'>Update Employee</h3>
                             <div className='card-body'>
                                 <form>
                                     <div className='form-group'>
@@ -68,8 +78,9 @@ class createEmployeeComponent extends Component {
                                         <input placeholder='Email Address' name='emailId' className='form-control'
                                         value={this.state.emailId} onChange={this.changeEmailHandler}/>
                                     </div>
-                                         <button className='btn btn-success' onClick={this.saveEmployee}>Save</button>
+                                         <button className='btn btn-success' onClick={this.updateEmployee}>Update</button>
                                          <button className='btn btn-danger' onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                                        
                                 </form>
                             </div>
                         </div>
@@ -79,5 +90,4 @@ class createEmployeeComponent extends Component {
         );
     }
 }
-
-export default createEmployeeComponent;
+export default UpdateEmployeeComponent;
